18c0457f8a004f30437b4a9ec29cc7c741751d1b
popina1994 <djordjezivanovicva@gmail.com>
2025-04-30 09:26:49 +0100

    1. Adding tests for dominate nodes.

--- test/logic/test_graph.py
+++ test/logic/test_graph.py
@@ -20,7 +20,19 @@ class TestDataFlowGraph(unittest.TestCase):
             self.assertEqual(dfg.can_reach_node_test_api(node_name, None), True)
 
 
-
+    def test_dominate_nodes(self):
+        list_node_names = ["R", "C", "F", "G", "I", "J", "K", "B", "E", "A", "H", "D", "L"]
+        list_edge_names = [("R", "C"), ("C", "F"), ("C", "G"), ("F", "I"), ("G", "I"), ("G", "J"), ("J", "I"), ("I", "K"), ("K", "I"), ("H", "K"),
+        ("R", "B"), ("R", "A"), ("B", "A"), ("B", "D"), ("B", "E"),
+        ("E", "H"), ("H", "E"), ("A", "D"), ("D", "L"), ("L", "H")]
+        dfg = DataFlowGraph(list_node_names, list_edge_names, "R")
+        map_node_name_dominator_names = {"R": [], "I": ["R"], "K": ["R"], "C": ["R"],
+                                        "H": ["R"], "E": ["R"], "A": ["R"], "D":["R"], "B": ["R"], "L": ["D", "R"],
+                                        "F": ["C", "R"], "G": ["C", "R"],
+                                        "J": ["C", "G", "R"]}
+        for node_name in list_node_names:
+            dominate_node_names = dfg.get_dominate_nodes(node_name)
+            self.assertEqual(set(dominate_node_names) == set(map_node_name_dominator_names[node_name]), True)
 
 if __name__ == '__main__':
     unittest.main()
